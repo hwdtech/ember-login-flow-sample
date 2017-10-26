3c17bb6a6b3dca5130a24439a4e50cb4ca6fdea0
Maxim Malov <mmalov@hwdtech.ru>
2017-10-26 16:12:29 +0600

    Initialize code style settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,21 @@
+const prettierConfig = require('./package.json').prettier;
+
 module.exports = {
+  globals: {
+    server: true
+  },
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:ember/recommended', 'prettier'],
+  plugins: ['prettier'],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', prettierConfig],
+    'ember/named-functions-in-promises': 'off'
   }
 };
