f65eb324bb4387063e38f3599581d5b4237e40d4
Daniel Safonov <vendetta995@gmail.com>
2017-11-01 09:34:09 -0400

    LF/1 Modify environment.js; Remove ESA deprecations

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import UnAuthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
 
 export default Route.extend(UnAuthenticatedRouteMixin, {
+  routeIfAlreadyAuthenticated: 'protected',
   resetController(controller) {
     this._super(...arguments);
     controller.reset();
