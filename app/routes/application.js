863236e3e91e2ce4c4a44819fc530a8b820be9e2
Daniel Safonov <vendetta995@gmail.com>
2017-11-01 10:08:06 -0400

    LF/1 Modify environment.js; Remove ESA deprecations (#11)

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,7 @@ import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 
 export default Route.extend(ApplicationRouteMixin, {
+  routeAfterAuthentication: 'protected',
   redirect() {
     this.transitionTo('protected');
   },
