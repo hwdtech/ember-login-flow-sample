70f969d0df455c97c08da0332d977863c9236828
Daniel Safonov <dsafonov@lineate.com>
2017-11-01 05:44:12 -0400

    LF/1 Add necessary options for auth-flow

--- app/mixins/check-authenticated-route-mixin.js
+++ app/mixins/check-authenticated-route-mixin.js
@@ -2,5 +2,7 @@ import Mixin from '@ember/object/mixin';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Mixin.create(AuthenticatedRouteMixin, {
-  authenticationRoute: 'login'
+  authenticationRoute: 'login',
+  routeAfterAuthentication: 'protected',
+  routeIfAlreadyAuthenticated: 'protected'
 });
