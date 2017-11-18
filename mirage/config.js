4922e4703348af203dc6848a676f0ee52410de26
Daniel Safonov <vendetta995@gmail.com>
2017-11-18 05:49:24 -0500

    LF/3 Add script for showing coverage results

--- mirage/config.js
+++ mirage/config.js
@@ -1,6 +1,7 @@
 import { createToken, revokeToken } from './routes/token';
 
 export default function() {
+  this.passthrough('/write-coverage');
   this.namespace = '/api'; // make this `/api`, for example, if your API is namespaced
 
   this.post('/token', createToken);
